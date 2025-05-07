050ff5422311e72e69c23334c84e411f0ba4d5fa
tapeds <frrllmatthew@gmail.com>
2025-05-07 09:05:36 +0700

    initial commit

---
+++ gabies-gallery/gabies_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  gabies_galleryApp.swift
+//  gabies-gallery
+//
+//  Created by Farrell Matthew Lim on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct gabies_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
